8e9a70be9d49478fb911101cd35567f56e271ed2
slyllama <alex.laing@me.com>
2025-08-05 10:50:56 +1200

    Improve player and camera handling

--- lib/_global/global.gd
+++ lib/_global/global.gd
@@ -10,11 +10,13 @@ var mouse_in_ui = false
 
 func _ready() -> void:
 	if DisplayServer.screen_get_size().x > 2000:
-		get_window().size *= 2.0
+		if !Engine.is_embedded_in_editor():
+			get_window().size *= 2.0
 		get_window().content_scale_factor = 2.0
 		
 		if OS.get_name() != "macos":
 			DisplayServer.cursor_set_custom_image(
 				load("res://generic/textures/cursor_2x.png"))
 	
-	get_window().position = DisplayServer.screen_get_position() + Vector2i(32, 64)
+	if !Engine.is_embedded_in_editor():
+		get_window().position = DisplayServer.screen_get_position() + Vector2i(32, 64)
--- lib/player/player.gd
+++ lib/player/player.gd
@@ -1,18 +1,8 @@
 extends CharacterBody3D
 
 @export var speed = 1.9
-@export var zoom_increment = 0.25
-@export var zoom_minimum = 1.0
-@export var zoom_maximum = 3.0
-
-@onready var _target_zoom = position.z
-
-func _input(_event: InputEvent) -> void:
-	# Handle zoom
-	if Input.is_action_just_pressed("zoom_in"):
-		_target_zoom -= zoom_increment
-	elif Input.is_action_just_pressed("zoom_out"):
-		_target_zoom += zoom_increment
+@export var acceleration = 9.0
+var _target_velocity = 0.0
 
 func _physics_process(_delta: float) -> void:
 	var _direction = Vector3.ZERO
@@ -22,18 +12,19 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("strafe_right"): _direction.x = 1.0
 	
 	_direction = _direction.normalized()
-	velocity = $Orbit.basis * Vector3.FORWARD * _direction.z
-	velocity += $Orbit.basis * Vector3.RIGHT * _direction.x
-	velocity *= speed
+	_target_velocity = $Orbit.basis * Vector3.FORWARD * _direction.z
+	_target_velocity += $Orbit.basis * Vector3.RIGHT * _direction.x
+	_target_velocity *= speed
+	velocity = lerp(velocity, _target_velocity, Utils.clerp(acceleration))
 	
 	var _y_diff = $YCast.global_position.y - $YCast.get_collision_point().y
 	velocity.y += clamp(Global.GRAVITY * _y_diff, Global.GRAVITY, 0.0)
 	
 	move_and_slide()
 	
-	# Handle zoom
-	_target_zoom = clamp(_target_zoom, zoom_minimum, zoom_maximum)
-	$Orbit/SpringAxis.spring_length = lerp($Orbit/SpringAxis.spring_length,
-		_target_zoom, Utils.clerp(24.0))
-	$Orbit/Camera.position.z = lerp($Orbit/Camera.position.z,
-		$Orbit/SpringAxis/Spring.position.z, Utils.clerp(20.0))
+	# Handle player mesh operations (facing rotation)
+	if velocity.length() > 0.1:
+		$PlayerMesh.rotation_degrees.y = lerp(
+			$PlayerMesh.rotation_degrees.y, $Orbit.rotation_degrees.y, Utils.clerp(20.0))
+	$PlayerMesh.rotation_degrees.z = lerp(
+		$PlayerMesh.rotation_degrees.z, velocity.x * -9.0, Utils.clerp(20.0))
